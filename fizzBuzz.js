f9495ff4b9a21d4f98fb5e8e26a8b156d04578bc
Josephine Obeng <josephine.obeng@hotmail.com>
2022-08-22 21:04:49 +0100

    Add 'new username' comment

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -4,6 +4,7 @@
 // print buzz for multiples of 5
 // print fizzBuzz for multiples of 3 and 5
 // otherwise return number
+// test if my new username comes up
 
 function fizzBuzz(a, b) {
   return a + b;
